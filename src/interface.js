79abfec87a2a7a8c6415bccc671b4e4cc9b87cd6
Tom <you@example.com>
2018-11-07 15:11:19 +0000

    added changes

--- src/interface.js
+++ src/interface.js
@@ -1,36 +1,36 @@
-        $(document).ready(function() {
-        var thermostat = new Thermostat();
+$(document).ready(function() {
+    var thermostat = new Thermostat();
+    updateTemperature();
+
+    $('#temp-up').click(function() {
+        thermostat.up();
         updateTemperature();
-
-        $('#temp-up').click(function() {
-            thermostat.increaseTemperature();
-            updateTemperature();
-        });
-
-        $('#temp-down').click(function() {
-            thermostat.decreaseTemperature();
-            updateTemperature();
-        });
-
-        $('#temp-reset').click(function() {
-            thermostat.reset();
-            updateTemperature();
-        });
-
-        $('#psm-on').click(function() {
-            thermostat.powerSavingModeOn();
-            $('#power-saving').text('on')
-            updateTemperature();
-        });
-
-        $('#psm-off').click(function() {
-            thermostat.powerSavingModeOff();
-            $('#power-saving').text('off')
-            updateTemperature();
-        });
-
-        function updateTemperature() {
-            $('#temperature').text(thermostat.temperature);
-        };
     });
 
+    $('#temp-down').click(function() {
+        thermostat.down();
+        updateTemperature();
+    });
+
+    $('#temp-reset').click(function() {
+        thermostat.resetTemperature();
+        updateTemperature();
+    });
+
+    $('#psm-on').click(function() {
+        thermostat.switchPowerSavingModeOn();
+        $('#power-saving').text('on');
+        updateTemperature();
+    })
+
+    $('#psm-off').click(function() {
+        thermostat.switchPowerSavingModeOff();
+        $('#power-saving').text('off');
+        updateTemperature();
+    });
+
+    function updateTemperature() {
+        $('#temperature').text(thermostat.temperature);
+        $('#temperature').attr('class', thermostat.energyUsage());
+    }
+});
