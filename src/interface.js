92dafa14bcfe3a22f005cc3a3ab4ea056236baad
Tom <you@example.com>
2018-11-07 14:28:07 +0000

    added content to interface

--- src/interface.js
+++ src/interface.js
@@ -1,55 +1,36 @@
-$(document).ready(function() {
-    var thermostat = new Thermostat();
-    $('#temperature').text(thermostat.temperature);
-})
-
-$('#temp-up').click(function() { // event listener
-    thermostat.increaseTemperature(); // update model
-    $('#temperature').text(thermostat.temperature); // update view
-})
-
-$('#temp-down').click(function() { // this is an alternate version of .on('click'), with a sprinkle of jQuery syntactic sugar
-    thermostat.decreaseTemperature();
-    $('#temperature').text(thermostat.temperature);
-})
-
-function updateTemperature() {
-    $('#temperature').text(thermostat.temperature);
-}
-
-$(document).ready(function() {
-    var thermostat = new Thermostat();
-    updateTemperature();
-
-    $('#temp-up').click(function() {
-        thermostat.increaseTemperature();
+        $(document).ready(function() {
+        var thermostat = new Thermostat();
         updateTemperature();
-    });
 
-    $('#temp-down').click(function() {
-        thermostat.decreaseTemperature();
-        updateTemperature();
-    });
+        $('#temp-up').click(function() {
+            thermostat.increaseTemperature();
+            updateTemperature();
+        });
 
-    $('#temp-reset').click(function() {
-        thermostat.reset();
-        updateTemperature();
-    });
+        $('#temp-down').click(function() {
+            thermostat.decreaseTemperature();
+            updateTemperature();
+        });
 
-    $('#psm-on').click(function() {
-        thermostat.powerSavingModeOn();
-        $('#power-saving').text('on')
-        updateTemperature();
-    });
+        $('#temp-reset').click(function() {
+            thermostat.reset();
+            updateTemperature();
+        });
 
-    $('#psm-off').click(function() {
-        thermostat.powerSavingModeOff();
-        $('#power-saving').text('off')
-        updateTemperature();
-    });
+        $('#psm-on').click(function() {
+            thermostat.powerSavingModeOn();
+            $('#power-saving').text('on')
+            updateTemperature();
+        });
 
-    function updateTemperature() {
-        $('#temperature').text(thermostat.temperature);
-    };
-});
+        $('#psm-off').click(function() {
+            thermostat.powerSavingModeOff();
+            $('#power-saving').text('off')
+            updateTemperature();
+        });
+
+        function updateTemperature() {
+            $('#temperature').text(thermostat.temperature);
+        };
+    });
 
